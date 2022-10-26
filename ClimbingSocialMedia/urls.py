7be3be1a470cc777f60848f51e44096f738ee4ba
brian campana <bcampana826@gmail.com>
2022-10-26 15:58:49 -0400

    Post Changes

--- ClimbingSocialMedia/urls.py
+++ ClimbingSocialMedia/urls.py
@@ -19,5 +19,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home)
+    path('', views.home),
+    path('home', views.posts)
 ]
--- ClimbingSocialMedia/views.py
+++ ClimbingSocialMedia/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
+from .models import UserProfile
+
 def home(request):
     return render(request, 'ClimbingSocialMedia/home.html')
+
+def posts(request):
+    return render(request, 'ClimbingSocialMedia/posts.html')
