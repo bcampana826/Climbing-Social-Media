47089ba4dfc3f757f3a34bf5d7507449ea50b4f9
Herb <117784256+enciso47@users.noreply.github.com>
2022-12-04 00:04:02 -0500

    Update models.py
    Updated models.py to include gym profiles

--- ClimbingSocialMedia/models.py
+++ ClimbingSocialMedia/models.py
@@ -8,10 +8,21 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     user = models.OneToOneField(User, null=False, on_delete=models.CASCADE)
     bio = models.CharField(max_length=500, blank=True)
-    picture = models.ImageField(default='defaultProfilePic.jpg', upload_to='profile_pics', null=True, blank=True)
+    picture = models.ImageField(default='defaultProfilePic.jpg', upload_to='user_profile_pictures', null=True, blank=True)
 
     def __str__(self):
-        return f'{self.user.username}\'s Profile'
+        return f'{self.user.username}\'s Personal Profile'
+    
+class GymProfile(models.Model):
+    user = models.OneToOneField(User, null=False, on_delete=models.CASCADE)
+    gym_name = models.CharField(max_length=50, null=False)
+    bio = models.CharField(max_length=500, blank=True)
+    picture = models.ImageField(default='defaultProfilePic.jpg', upload_to='gym_profile_pictures', null=True,blank=True)
+    verified = models.BooleanField(default=False)
+    location = models.CharField(max_length=50, blank=True)
+    
+    def __str__(self):
+        return f'{self.user.username}\'s Gym Profile'
 
 class Comment(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
