38f72ebe8e5befc96273e5cd706beb7974ce5e80
Herb <117784256+enciso47@users.noreply.github.com>
2022-12-04 00:06:36 -0500

    Update urls.py

--- ClimbingSocialMedia/urls.py
+++ ClimbingSocialMedia/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('report/',views.report,name="Report"),
     path('logout/', views.logout_view, name="logout"),
     path('profile/', views.profile, name="Profile"),
+    path('info/',views.per_info,name='PersonalInfo')
 ]
 
 if settings.DEBUG:
