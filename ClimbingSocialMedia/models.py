350dbbb67da51a7bdc0b8c64403723c02a42e874
Herb <enciso47@students.rowan.edu>
2022-12-09 22:20:41 -0500

    Added a resizer if image is larger than 500 pixels

--- ClimbingSocialMedia/models.py
+++ ClimbingSocialMedia/models.py
@@ -3,6 +3,7 @@ import uuid
 
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 class UserProfile(models.Model):
@@ -17,6 +18,16 @@ class UserProfile(models.Model):
     following = models.ManyToManyField(User, related_name="user_following", blank=True)
     def __str__(self):
         return f'{self.user.username}\'s Personal Profile'
+    ## Resizes the profile picture to 500x500 pixels, so big size images dont take a lot of space.
+    def save(self, *args, **kwargs):
+        super().save()
+
+        img = Image.open(self.picture.path)
+
+        if img.height > 500 or img.width > 500:
+            new_img = (500, 500)
+            img.thumbnail(new_img)
+            img.save(self.picture.path)
   
 
 class Comment(models.Model):
